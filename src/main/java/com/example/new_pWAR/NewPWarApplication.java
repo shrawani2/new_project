fa4303882e9abb375e3fc550bcc27c9c7c3e8e49
Shrawani <shrawani@gmail.com>
2025-01-13 20:19:24 +0530

    New Project

---
+++ src/main/java/com/example/new_pWAR/NewPWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_pWAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewPWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new_pWAR/NewPWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_pWAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
